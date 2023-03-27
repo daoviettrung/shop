d0bd8f5ed89cdb0c2405ca93c65d7eeb656767c9
dvtrung <viettrung662000@gmail.com>
2023-03-27 12:08:15 +0700

    Checkout blade

---
+++ database/migrations/2023_03_26_083632_create_bills_table.php
@@ -0,0 +1,38 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateBillsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('bills', function (Blueprint $table) {
+            $table->id();
+            $table->integer('user_order_id');
+            $table->string('recipient_name');
+            $table->integer('city_id');
+            $table->integer('district_id');
+            $table->string('number_phone');
+            $table->decimal('price', $precision = 10, $scale = 2);
+            $table->integer('deleted');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('bills');
+    }
+}
