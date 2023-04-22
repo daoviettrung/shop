57b68eb8e56454a5ca4375972725fd161e3f5e84
dvtrung <viettrung662000@gmail.com>
2023-04-22 21:34:15 +0700

    Checkout blade

--- app/Http/Controllers/Admin/DashboardController.php
+++ app/Http/Controllers/Admin/DashboardController.php
@@ -3,9 +3,12 @@
 namespace App\Http\Controllers\Admin;
 
 use App\Http\Controllers\Controller;
+use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Artisan;
 use Illuminate\Support\Facades\Cache;
 use Illuminate\Support\Facades\Log;
+use Illuminate\Support\Facades\DB;
+
 
 class DashboardController extends Controller
 {
@@ -14,9 +17,26 @@ class DashboardController extends Controller
      *
      * @return \Illuminate\Http\Response
      */
-    public function index()
+    public function index(Request $request)
     {
-        return view('admin.pages.dashboard.index');
+        $filterYear = config('constants.filter_years');
+        $timeNow = substr(date("Y-m-d",time()),0, 4);
+        $year = (empty($request->filter_year)) ? $timeNow : $request->filter_year;
+        $dataRevenue = DB::table('bills')
+            ->where('deleted', '=', 0)
+            ->whereYear('created_at', '=', $year)
+            ->get();
+        $monthNumber = config('constants.month');
+        $data = [];
+        foreach ($monthNumber as $valueMonth){
+            $data[$valueMonth] = 0;
+            foreach ($dataRevenue as $valueData){
+                if((int)substr($valueData->created_at,5, 2) == (int)$valueMonth){
+                    $data[$valueMonth] += $valueData->price;
+                }
+            }
+        }
+        return view('admin.pages.dashboard.index', compact('filterYear','timeNow', 'data','year'));
     }
 
     public function cacheClear()
--- resources/views/admin/pages/dashboard/index.blade.php
+++ resources/views/admin/pages/dashboard/index.blade.php
@@ -13,14 +13,91 @@
             <div class="overview-wrap">
                 <h2 class="title-1">Dashboard</h2>
                 <button id='cache-clear' class="au-btn au-btn-icon au-btn--blue">
-                    <i class="zmdi zmdi-calendar-remove"></i>Clear cache</button>
+{{--                    <i class="zmdi zmdi-calendar-remove"></i>Clear cache</button>--}}
+            </div>
+        </div>
+    </div>
+
+    <div class="row">
+        <div class="col-md-12">
+            <div class="report-dashboard">
+                <form action="{{ url('dashboard') }}" method="GET">
+                        <select id="filter_year" class="filter-year" name="filter_year" onchange="this.form.submit()">
+                        @foreach($filterYear as $key => $value)
+                           <option @if($key == $year) selected @endif value="{{$key}}">{{$value}}</option>
+                            @endforeach
+                    </select>
+                </form>
+            </div>
+        </div>
+    </div>
+    <div class="row">
+        <div class="col-md-12">
+            <div class="overview-wrap">
+                <canvas id="myChart" style="width:100%;min-width:700px"></canvas>
             </div>
         </div>
     </div>
 @endsection
 
 @section('script')
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/Chart.js/2.9.4/Chart.js"></script>
     <script type="text/javascript">
+        const MONTHS = [
+            'January',
+            'February',
+            'March',
+            'April',
+            'May',
+            'June',
+            'July',
+            'August',
+            'September',
+            'October',
+            'November',
+            'December'
+        ];
+        const labels = months();
+        var dataChart = @json($data);
+        const data = {
+            labels: labels,
+            datasets: [{
+                label: 'Revenue data',
+                data: Object.values(dataChart),
+                backgroundColor: 'rgba(255, 99, 132, 0.2)',
+                borderColor: 'rgb(255, 99, 132)',
+                borderWidth: 1
+            }]
+        };
+        const config = {
+
+        };
+        const myChart = new Chart("myChart", {
+            type: 'bar',
+            data: data,
+            options: {
+                scales: {
+                    y: {
+                        beginAtZero: true
+                    }
+                }
+            },
+        });
+
+        function months(config) {
+            var cfg = config || {};
+            var count = cfg.count || 12;
+            var section = cfg.section;
+            var values = [];
+            var i, value;
+
+            for (i = 0; i < count; ++i) {
+                value = MONTHS[Math.ceil(i) % 12];
+                values.push(value.substring(0, section));
+            }
+
+            return values;
+        }
         $("#cache-clear").click(function() {
             $.ajax({
                 url: 'dashboard/cache-clear',
